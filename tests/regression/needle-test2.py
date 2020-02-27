917d5fbcb6ce2d793758d1830879ad2202d16ffb
BoringMind <super.d3280@ya.ru>
2020-02-27 21:01:44 +0300

    Written screen maker module. Refactoring

--- tests/regression/needle-test2.py
+++ tests/regression/needle-test2.py
@@ -1,12 +1,5 @@
-from needle.cases import NeedleTestCase
-# from needle.driver import NeedleFirefox
-from needle.driver import NeedleChrome
 from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.keys import Keys
-
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
@@ -30,40 +23,93 @@ class element_is_not_visible(object):
         else:
             return element
 
-        # options = webdriver.chrome.options.Options()
-        # options.add_argument("--headless")
-        # return NeedleChrome(chrome_options=options)
 
-def default_regr_test_timing(pagename="index", url=""):
-    # print('Enter test')
-    time1 = perf_counter()
+def wait_for_menu(driver: webdriver, delay=3):
+    """Wait until categories menu is closed.
 
-    options = webdriver.chrome.options.Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    
-    max_height = driver.execute_script(
-        'return document.body.parentNode.scrollHeight'
-    )
-    driver.set_window_size(1920, max_height)
-    # print('Driver is set')
-    driver.get(r'http://127.0.0.1:8000/' + url)
-    delay = 3
+    delay - max time to wait, in seconds
+    """
     try:
-        # print('enter try block')
         element_invisible = element_is_not_visible(
             (By.CLASS_NAME, 'menu-hidden')
         )
         WebDriverWait(driver, delay).until(
             element_invisible
         )
-        # print('Element is loaded successfully')
     except TimeoutException:
         print('Page loading took too much time')
 
-    time2 = perf_counter()
-    print(time2 - time1)
-    # print('Test is done')
+
+def get_page_height(driver: webdriver) -> int:
+    """Get full height of webpage."""
+    return int(driver.execute_script(
+        'return document.body.parentNode.scrollHeight'
+    ))
 
 
-default_regr_test_timing('login', 'login/')
+def take_full_page_screenshot(
+    driver: webdriver,
+    file_name="screenshot",
+    wait_func=wait_for_menu,
+    page_width=1920
+):
+    """Get full page screenshot.
+
+    file_name - name of output screenshot file, without .jpg or .png extension
+    wait_func - function, which implements imlicit wait for selenium
+    page_width - browser screen width
+
+    Screenshots are saved in the same folder with script.
+    Files have png format.
+    """
+    page_height = get_page_height(driver)
+    driver.set_window_size(page_width, page_height)
+
+    wait_func(driver)
+
+    driver.find_element_by_tag_name('body').screenshot(file_name + ".png")
+
+
+def get_chrome_driver() -> webdriver:
+    """Set chrome driver to headless mode and return instance."""
+    options = webdriver.chrome.options.Options()
+    options.add_argument("--headless")
+    options.add_argument("--start-maximized")
+    return webdriver.Chrome(options=options)
+
+
+def get_firefox_driver() -> webdriver:
+    """Set firefox driver to headless mode and return instance."""
+    options = webdriver.FirefoxOptions()
+    options.set_headless()
+    return webdriver.Firefox(options=options)
+
+
+def benchmark(function, *params):
+    """Decorator. Benchmarks function and prints elapsed time.
+
+    function - function to be benchmarked
+    *params - parameters, passed to wrapped function
+    """
+    def wrapper(*params):
+        time1 = perf_counter()
+        function(*params)
+        time2 = perf_counter()
+        print(time2 - time1)
+    return wrapper
+
+
+@benchmark
+def regression_test(pagename="index", url=""):
+    try:
+        driver = get_chrome_driver()
+        driver.get(r'http://127.0.0.1:8000/' + url)
+        take_full_page_screenshot(driver, pagename)
+    except Exception as e:
+        raise e
+    finally:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    regression_test('login', 'login/')
